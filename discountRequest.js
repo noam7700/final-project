8c7360ddedd07d3e62a8c7753d3d8e189241fd3f
Noam Blagovsky <noam7700@gmail.com>
2019-02-17 10:42:16 +0200

    get the discount for specific product - discountRequest.js
    .js that get the response, but doesn't get the actual discount from the response (the response is some html object that has other things)
    
    --next we should add a function/code that deals with that response

---
+++ discountRequest.js
@@ -0,0 +1,52 @@
+const puppeteer = require('puppeteer');
+
+async function run() {
+    let browser = await puppeteer.launch({headless: false});
+    let page = await browser.newPage();
+    await page.setRequestInterception(true); //set the request option (triggered with goto)
+    page.on('request', req => {
+        //create data
+        let prodID = 7296073231578; //shoko
+        let qty = 2;
+        let remarks = '';
+        let unitofmeasure = '';
+        var request = {
+            AjaxCallAction: "AddProductToBasket",
+            paramProductID: prodID,
+            paramQuantity: qty,
+            paramRemarks: remarks,
+            paramUM: unitofmeasure
+        }
+        // convert JSON to x-www-form-urlencoded
+        let reqBody = Object.keys(request).map((k) => {
+            return encodeURIComponent(k) + '=' + encodeURIComponent(request[k])
+        }).join('&');
+        var data = {
+            'method': 'POST',
+            'url': 'https://www.shufersal.co.il/_layouts/Shufersal_Pages/ajax.aspx',
+             // the custom headers
+            'headers': {
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'X-Requested-With': 'XMLHttpRequest'
+            },
+            'postData': reqBody
+        };
+        req.continue(data);
+    });
+    // capture intercepted response
+    page.on('response', async response => {
+        console.log("Resource Type: "  + response.request().resourceType());
+        console.log("Response Text: " + await response.text());
+        console.log("==============");
+    });
+
+    const response = await page.goto('https://www.shufersal.co.il/_layouts/Shufersal_Pages/ajax.aspx');
+    // only capture default response
+    //console.log(response.statusText());
+    //console.log(response.responseText);
+
+    //console.log('done');
+
+}
+
+run();
