3d5fd29143c2c3736423b4766a4ca2a4688cbbed
Noam Blagovsky <noam7700@gmail.com>
2019-02-17 15:21:47 +0200

    added empty BasketActivity. just checking how commiting from android stuido works

---
+++ app/src/main/java/com/example/testingapp/BasketActivity.java
@@ -0,0 +1,13 @@
+package com.example.testingapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BasketActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_basket);
+    }
+}
