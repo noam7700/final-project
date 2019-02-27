c8727772d89d43a77011cbe6c3b4c77571897069
Noam Blagovsky <noam7700@gmail.com>
2019-02-27 10:44:43 +0200

    added button "Save basket" that does nothing...

--- app/src/main/java/com/example/testingapp/BasketActivity.java
+++ app/src/main/java/com/example/testingapp/BasketActivity.java
@@ -25,16 +25,28 @@ public class BasketActivity extends AppCompatActivity {
         TextView activity_basket_titleTextView = (TextView) findViewById(R.id.activity_basket_titleTextView);
         TextView activity_basket_sumTextView = (TextView) findViewById(R.id.activity_basket_sumTextView);
         Button activity_basket_buyBtn = (Button) findViewById(R.id.activity_basket_buyBtn);
+        Button activity_basket_saveBasketBtn = (Button) findViewById(R.id.activity_basket_saveBasketBtn);
         myBasketListView = (ListView) findViewById(R.id.activity_basket_basketListView);
 
         activity_basket_titleTextView.setText("הסל שלי");
         activity_basket_sumTextView.setText("0.00$"); //TODO: maintain sumTextView
+
+        activity_basket_buyBtn.setText("בצע קנייה");
         activity_basket_buyBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //TODO: show on screen all the bought products? :/
             }
         });
+
+        activity_basket_saveBasketBtn.setText("שמור סל");
+        activity_basket_saveBasketBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: save made basket to the server. and update "My Baskets"
+            }
+        });
+
 
         ProductBasketAdapter productBasketAdapter = new ProductBasketAdapter(this, BasketActivity.basketProducts);
         myBasketListView.setAdapter(productBasketAdapter);
