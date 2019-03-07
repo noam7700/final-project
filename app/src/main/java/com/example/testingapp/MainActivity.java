9868907f09835840cbae40f29243222a7605b705
Noam Blagovsky <noam7700@gmail.com>
2019-03-07 16:51:41 +0200

    just adding "onNavigationItemSelectedListener" to menu
    I WANT ANIMATION OR SOME REACTION WHEN CLICKED >:(

--- app/src/main/java/com/example/testingapp/MainActivity.java
+++ app/src/main/java/com/example/testingapp/MainActivity.java
@@ -4,6 +4,8 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -28,6 +30,30 @@ public class MainActivity extends AppCompatActivity {
         mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+        //navigation menu
+        NavigationView navigation_menu = (NavigationView) findViewById(R.id.navigation_view);
+        navigation_menu.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Intent startIntent;
+                switch(menuItem.getItemId()){
+                    case R.id.nav_main:
+
+                        startIntent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(startIntent);
+                    case R.id.nav_menu:
+                        startIntent = new Intent(getApplicationContext(), MenuActivity.class);
+                        startActivity(startIntent);
+                        return true;
+                    case R.id.nav_login:
+                        startIntent = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(startIntent);
+                    default:
+                        return false;
+                }
+
+            }
+        });
 
         if(BasketActivity.currentBasket == null){
             BasketActivity.currentBasket = new Basket();
@@ -55,9 +81,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if(mToggle.onOptionsItemSelected(item)){
+        if(mToggle.onOptionsItemSelected(item)){ //if it's
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 
