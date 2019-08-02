25456af918cc62438bfe7f6abe3462eb19f52044
Noam Blagovsky <noam7700@gmail.com>
2019-08-02 23:43:06 +0300

    little change

--- app/src/main/java/com/example/testingapp/OcrManager.java
+++ app/src/main/java/com/example/testingapp/OcrManager.java
@@ -1,5 +1,6 @@
 package com.example.testingapp;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
@@ -8,12 +9,14 @@ public class OcrManager {
 
     private TessBaseAPI tessBaseAPI;
 
-    private String getText(Bitmap bitmap, String dataPath){
+    private String getText(Bitmap bitmap, Context context){
         try{
             tessBaseAPI = new TessBaseAPI();
         }catch (Exception e){
             e.printStackTrace();
         }
+
+        String dataPath = context.getExternalFilesDir("/").getPath() + "/";
 
         tessBaseAPI.init(dataPath, "heb");
         tessBaseAPI.setImage(bitmap);
